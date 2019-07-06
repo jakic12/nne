91916a4800e39425003c15ce37fd88da66dd7c5b
jakic12 <drusany.jakob@gmail.com>
2019-07-06 12:14:09 +0200

    added food, world wrapping

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 import Species from './Species.js'
 import Animal from './Animal.js'
+import Food from './Food.js'
 
 var canvas = document.getElementById('mainCanvas'); canvas.width = document.body.clientWidth; canvas.height = document.body.clientHeight
 var ctx = canvas.getContext("2d");
 
 let animals = [];
+let food = [];
 
 /*let minorSpecies = new Species();
 for (let i = 0; i < 100; i++) {
@@ -21,6 +23,10 @@ for(let foodChainEval = 0; foodChainEval < 5; foodChainEval++){
         animals.push(new Animal(Math.random()*canvas.width,Math.random()*canvas.height,newspecies));
     }
 }
+
+for(let i = 0; i < 100; i++){
+    food.push(new Food(Math.random()*canvas.width, Math.random()*canvas.height));
+}
 /*let params = animals[0].getInputParameters(animals);
 ctx.beginPath()
 ctx.moveTo(animals[0].x, animals[0].y)  
@@ -31,15 +37,25 @@ function mainLoop(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     let eatenAnimals = [];
+    let eatenFood = [];
+
+    food.forEach((f) => {
+        f.draw(ctx);
+    });
 
     animals.forEach((animal) => {
-        animal.calculateMovement(animals);  
+        animal.calculateMovement(animals, food);  
         animal.updateAnimal();
         animal.drawAnimal(ctx);
 
         if(animal.canEat){
-            eatenAnimals.push(animal.canEat);
-            animal.foodInventory += animal.canEat.foodInventory + 1;
+            if(animals.includes(animal.canEat)){
+                eatenAnimals.push(animal.canEat);
+                animal.foodInventory += animal.canEat.foodInventory + 1;
+            }else{
+                eatenFood.push(animal.canEat);
+                animal.foodInventory += 1;
+            }
         }
 
         if(animal.canMate){
@@ -50,29 +66,42 @@ function mainLoop(){
                 animals.push(offsprings);
         }
 
-        if(animal.x > canvas.width - animal.size){
-            animal.x = canvas.width - animal.size;
+        if(animal.x > canvas.width + animal.size){
+            animal.x = animal.size;
         }
 
-        if(animal.x < animal.size){
-            animal.x = animal.size
+        if(animal.x < -animal.size){
+            animal.x = canvas.width - animal.size
         }
 
-        if(animal.y > canvas.height - animal.size){
-            animal.y = canvas.height - animal.size;
+        if(animal.y > canvas.height + animal.size){
+            animal.y = animal.size;
         }
 
         if(animal.y < animal.size){
-            animal.y = animal.size
+            animal.y = canvas.height - animal.size
         }
 
     })
 
     animals = animals.filter(animal => 
-        animal.health > 0 && !eatenAnimals.includes(animal)
+        animal.health > 0 &&
+        !eatenAnimals.includes(animal)/* &&
+        animal.x < canvas.width + animal.size &&
+        animal.x > -animal.size &&
+        animal.y < canvas.height + animal.size &&
+        animal.y > -animal.size*/
+    )
+
+    food = food.filter(f => 
+        !eatenFood.includes(f)    
     )
     
     window.requestAnimationFrame(mainLoop);
 }
+
+setInterval(() => {
+    food.push(new Food(Math.random()*canvas.width, Math.random()*canvas.height));
+}, 500)
 
 window.requestAnimationFrame(mainLoop);
